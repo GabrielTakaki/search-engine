f5f8a46de9b59a42e9f5cfe0a4b98f37bc81b70a
gabrieltakaki <gabitj@icloud.com>
2024-09-07 20:30:42 -0300

    feat: it creates ListItemButton component

--- frontend/src/components/data-display/List.jsx
+++ frontend/src/components/data-display/List.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import MuiList from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import MuiListItemButton from "@mui/material/ListItemButton";
+import { styled } from "@mui/material/styles";
+import { COLORS } from "../../consts/colors";
+import { ListItemIcon } from "@mui/material";
 
 function List({ datasource, keyExtractor, renderItems, renderAction }) {
   return (
@@ -19,11 +23,40 @@ function List({ datasource, keyExtractor, renderItems, renderAction }) {
   );
 }
 
+const StyledListItemButton = styled(MuiListItemButton)({
+  borderRadius: 8,
+  padding: 4,
+  width: 38,
+  height: 38,
+  backgroundColor: COLORS.primary.lighter
+});
+
+const StyledListItemIcon = styled(ListItemIcon)({
+  minWidth: "unset",
+  justifyContent: "center",
+  margin: "auto",
+  color: COLORS.primary.darkest
+});
+
+function ListItemIconButton({ icon }) {
+  return (
+    <StyledListItemButton>
+      <StyledListItemIcon>{icon}</StyledListItemIcon>
+    </StyledListItemButton>
+  );
+}
+
 List.propTypes = {
   datasource: PropTypes.array.isRequired,
   renderItems: PropTypes.func.isRequired,
   renderAction: PropTypes.func,
   keyExtractor: PropTypes.func.isRequired
 };
+
+ListItemIconButton.propTypes = {
+  icon: PropTypes.node.isRequired
+};
+
+List.ListItemIconButton = ListItemIconButton;
 
 export default List;
