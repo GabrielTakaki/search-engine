460890974237facc7bf68b4f77df01121624db9d
gabrieltakaki <gabitj@icloud.com>
2024-09-08 14:22:53 -0300

    refactor: it memoizes the params

--- frontend/src/hooks/useService.js
+++ frontend/src/hooks/useService.js
@@ -1,37 +1,36 @@
 import { useCallback, useState, useEffect, useRef } from "react";
 
-export default function useService(service, { autoStart, params, onData, onError }) {
-  const [isFetching, setIsFetching] = useState(!!autoStart || false);
+export default function useService(service, { autoStart = false, params = {}, onData, onError }) {
+  const [isFetching, setIsFetching] = useState(!!autoStart);
   const isMounted = useRef(true);
 
-  const start = useCallback(
-    async (params) => {
-      try {
-        setIsFetching(true);
-        const result = await service(params);
+  const start = useCallback(async () => {
+    try {
+      setIsFetching(true);
+      const result = await service(params);
+      if (isMounted.current) {
         onData(result);
-      } catch (error) {
-        if (onError) {
-          onError(error);
-        }
-      } finally {
-        if (isMounted.current) {
-          setIsFetching(false);
-        }
       }
-    },
-    [service, params, isMounted]
-  );
+    } catch (error) {
+      if (isMounted.current && onError) {
+        onError(error);
+      }
+    } finally {
+      if (isMounted.current) {
+        setIsFetching(false);
+      }
+    }
+  }, [service, onData, onError, params]);
 
   useEffect(() => {
     if (autoStart) {
-      start(params);
+      start();
     }
 
     return () => {
       isMounted.current = false;
     };
-  }, []);
+  }, [autoStart, start]);
 
   return [isFetching, start];
 }
