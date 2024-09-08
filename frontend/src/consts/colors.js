1fdc1ae95662aa424f1a57f149f4bdb304695339
gabrieltakaki <gabitj@icloud.com>
2024-09-07 22:58:03 -0300

    feat: system colors const

---
+++ frontend/src/consts/colors.js
@@ -0,0 +1,14 @@
+export const COLORS = {
+  primary: {
+    lightest: "#E4F6ED",
+    lighter: "#BDE9D2",
+    default: "#26C281",
+    darkest: "#008347"
+  },
+  neutral: {
+    white: "#FFFFFF",
+    lightGray: "#8C9196",
+    gray: "#6F7170",
+    black: "#0F1310"
+  }
+};
