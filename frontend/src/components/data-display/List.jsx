2ae7e0f3dd331ab37e28e303c9f4ec2d8aeb7e4c
gabrieltakaki <gabitj@icloud.com>
2024-09-09 20:28:45 -0300

    refactor: it uses styled from styles components rather the one from mui

--- frontend/src/components/data-display/List.jsx
+++ frontend/src/components/data-display/List.jsx
@@ -1,53 +1,82 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import MuiList from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import MuiListItemButton from "@mui/material/ListItemButton";
-import { styled } from "@mui/material/styles";
+import { styled } from "styled-components";
 import { COLORS } from "../../consts/colors";
-import { ListItemIcon } from "@mui/material";
+import Pagination from "../navigation/Pagination";
 
-function List({ datasource, keyExtractor, renderItems, renderAction }) {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  width: 100%;
+`;
+
+const PaginationWrapper = styled.div`
+  align-items: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+function List({ datasource, keyExtractor, renderItems, renderAction, pagination, onChange }) {
+  const handlePageChange = useCallback(
+    (page) => {
+      onChange?.(page);
+    },
+    [onChange]
+  );
+
   return (
-    <MuiList>
-      {datasource.map((data) => (
-        <ListItem
-          secondaryAction={renderAction?.(data)}
-          key={keyExtractor(data)}
-          disablePadding
-          sx={{ display: "block" }}>
-          {renderItems(data)}
-        </ListItem>
-      ))}
-    </MuiList>
+    <Container>
+      <MuiList>
+        {datasource.map((data) => (
+          <ListItem
+            secondaryAction={renderAction?.(data)}
+            key={keyExtractor(data)}
+            disablePadding
+            sx={{ display: "block" }}>
+            {renderItems(data)}
+          </ListItem>
+        ))}
+      </MuiList>
+      {pagination && (
+        <PaginationWrapper>
+          <Pagination onChange={handlePageChange} count={pagination.totalPages} />
+        </PaginationWrapper>
+      )}
+    </Container>
   );
 }
 
-const StyledListItemButton = styled(MuiListItemButton)({
-  borderRadius: 8,
-  padding: 4,
-  width: 38,
-  height: 38,
+const StyledListItemButton = styled(MuiListItemButton)`
+  border-radius: 8px !important;
+  padding: 4px !important;
+  width: 38px !important;
+  height: 38px !important;
 
-  "> .MuiListItemIcon-root": {
-    color: COLORS.primary.light
-  },
+  > .MuiListItemIcon-root {
+    color: ${COLORS.primary.light};
+  }
 
-  "&.Mui-selected": {
-    backgroundColor: COLORS.primary.light,
-    color: COLORS.primary.darkest,
+  &.Mui-selected {
+    background-color: ${COLORS.primary.light};
+    color: ${COLORS.primary.darkest};
 
-    "> .MuiListItemIcon-root": {
-      color: COLORS.primary.darkest
+    > .MuiListItemIcon-root {
+      color: ${COLORS.primary.darkest};
     }
   }
-});
+`;
 
-const StyledListItemIcon = styled(ListItemIcon)({
-  minWidth: "unset",
-  justifyContent: "center",
-  margin: "auto"
-});
+const StyledListItemIcon = styled(ListItemIcon)`
+  min-width: unset !important;
+  justify-content: center !important;
+  margin: auto !important;
+`;
 
 function ListItemIconButton({ icon, selected }) {
   return (
@@ -61,6 +90,13 @@ List.propTypes = {
   datasource: PropTypes.array.isRequired,
   renderItems: PropTypes.func.isRequired,
   renderAction: PropTypes.func,
+  onChange: PropTypes.func,
+  pagination: PropTypes.shape({
+    page: PropTypes.number.isRequired,
+    perPage: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    totalPages: PropTypes.number.isRequired
+  }),
   keyExtractor: PropTypes.func.isRequired
 };
 
