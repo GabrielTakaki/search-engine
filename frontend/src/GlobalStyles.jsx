a90b9308afd76a7094d8da9d2ee9f3cee2fc4764
gabrieltakaki <gabitj@icloud.com>
2024-09-10 15:01:14 -0300

    refactor: it uses the google inter font from google's library

--- frontend/src/GlobalStyles.jsx
+++ frontend/src/GlobalStyles.jsx
@@ -1,25 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  @font-face {
-    font-family: "Inter";
-    src: url('./assets/fonts/Inter-Light.woff2') format('woff');
-    font-weight: 400;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: "Inter";
-    src: url('./assets/fonts/Inter-Medium.woff2') format('woff');
-    font-weight: 500;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: "Inter";
-    src: url('./assets/fonts/Inter-SemiBold.woff2') format('woff');
-    font-weight: 600;
-    font-style: normal;
-  }
-  
   * {
     font-family: Inter, sans-serif !important;
     font-variant-numeric: lining-nums !important;
@@ -27,5 +8,9 @@ export default createGlobalStyle`
   
   body {
     margin: 0;
+  }
+  
+  .MuiOutlinedInput-notchedOutline {
+      border: none !important;
   }
 `;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 import GlobalStyles from "./GlobalStyles";
+import utc from "dayjs/plugin/utc";
+import dayjs from "dayjs";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+
+dayjs.extend(utc);
 
 const root = createRoot(document.getElementById("root"));
 
