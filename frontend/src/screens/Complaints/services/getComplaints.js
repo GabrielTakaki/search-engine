423d839002bf65d1971240a30cd2cd2eb4262c5a
gabrieltakaki <gabitj@icloud.com>
2024-09-11 09:08:42 -0300

    feat: sortBy option

--- frontend/src/screens/Complaints/services/getComplaints.js
+++ frontend/src/screens/Complaints/services/getComplaints.js
@@ -1,12 +1,13 @@
 import { request } from "../../../helpers/request";
 
-export function getComplaints({ page, perPage, ...filters }) {
+export function getComplaints({ page, perPage, sortBy, ...filters }) {
   return request({
     method: "GET",
     url: "complaints",
     params: {
       page,
       per_page: perPage,
+      ...(sortBy ? { sort_by: sortBy } : {}),
       ...filters
     }
   });
